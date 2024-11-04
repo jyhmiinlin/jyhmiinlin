6b636bdd450b412ffc963d9c5d12577db63bd7b8
cedar-pine-user <89292823+mezmay@users.noreply.github.com>
2024-11-04 19:31:26 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 project = "林智敏醫師"
 copyright = "2024, JML team"
-author = "JML team"
+author = "JML team and ChatGPT"
 
 
 # -- General configuration ---------------------------------------------------
